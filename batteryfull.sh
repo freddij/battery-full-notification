97997ba60c62321a13a3637db7c5d5e29e674f09
freddijanto <freddijanto@gmail.com>
2020-01-03 20:48:13 +0700

    add low battery warning

--- batteryfull.sh
+++ batteryfull.sh
@@ -4,8 +4,13 @@ do
     export DISPLAY=:0.0
     battery_percent=$(cat /sys/class/power_supply/BAT0/capacity)
     if on_ac_power; then
-        if [ "$battery_percent" -gt 85 ]; then
+        if [ "$battery_percent" -gt 88 ]; then
             notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
+            paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
+        fi
+    else
+        if [ "$battery_percent" -lt 22 ]; then
+            notify-send -i "$PWD/batteryfull.png" "Battery empty." "Level: ${battery_percent}% "
             paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
         fi
     fi
